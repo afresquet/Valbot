cfe7c3a3794c9c7374ca3c99478ce131e684a161
Alvaro Fresquet <alvarofresquet@gmail.com>
2019-07-08 20:45:46 -0300

    Initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+	if (msg.content === "ping") {
+		msg.reply("pong");
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
