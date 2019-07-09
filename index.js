861a9dc2cbe1c4ea87f172c6b8e4c39fb4c961fe
Alvaro Fresquet <alvarofresquet@gmail.com>
2019-07-09 17:59:21 -0300

    Made server emojis work and cleaned up

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ client.on("raw", packet => {
 
 // Suggestions reactions
 client.on("message", async message => {
+	if (message.author.bot) return;
+
 	if (message.channel.name !== "suggestions") return;
 
 	await message.react("✅");
@@ -66,22 +68,42 @@ client.on("message", async message => {
 });
 
 // Roles
+const regex = /^\s*([^\s]+)\s+(\w+)\s+\|\s+(.+)/;
+const roleExtractor = message => {
+	const lines = message.content.split("\n");
+	const roleLines = lines.filter(line => regex.test(line));
+
+	const roles = roleLines.map(roleLine => {
+		const [emojiRaw, name, description] = regex.exec(roleLine).splice(1, 3);
+
+		let emoji = emojiRaw;
+
+		if (/<:.+:(\d+)>/.test(emojiRaw)) {
+			emoji = message.guild.emojis.get(/<:.+:(\d+)>/.exec(emoji)[1]);
+		}
+
+		return { name, description, emoji };
+	});
+
+	return roles;
+};
 client.on("messageReactionAdd", async (reaction, user) => {
+	if (user.bot) return;
+
 	if (reaction.message.channel.name !== "roles") return;
 
-	const roles = reaction.message.content
-		.split("\n")
-		.splice(2)
-		.map(role => role.split(" "));
+	const roles = roleExtractor(reaction.message);
 
-	const role = roles.find(([emoji]) => emoji === reaction.emoji.name);
+	const role = roles.find(
+		({ emoji }) => (emoji.name || emoji) === reaction.emoji.name
+	);
 
 	if (!role) return;
 
-	let serverRole = reaction.message.guild.roles.find(r => r.name === role[1]);
+	let serverRole = reaction.message.guild.roles.find(r => r.name === role.name);
 
 	if (!serverRole) {
-		serverRole = await reaction.message.guild.createRole({ name: role[1] });
+		serverRole = await reaction.message.guild.createRole({ name: role.name });
 	}
 
 	const guildMember = reaction.message.guild.member(user);
@@ -93,18 +115,19 @@ client.on("messageReactionAdd", async (reaction, user) => {
 	await user.send(`I gave you the "${serverRole.name}" role.`);
 });
 client.on("messageReactionRemove", async (reaction, user) => {
+	if (user.bot) return;
+
 	if (reaction.message.channel.name !== "roles") return;
 
-	const roles = reaction.message.content
-		.split("\n")
-		.splice(2)
-		.map(role => role.split(" "));
+	const roles = roleExtractor(reaction.message);
 
-	const role = roles.find(([emoji]) => emoji === reaction.emoji.name);
+	const role = roles.find(
+		({ emoji }) => (emoji.name || emoji) === reaction.emoji.name
+	);
 
 	if (!role) return;
 
-	let serverRole = reaction.message.guild.roles.find(r => r.name === role[1]);
+	let serverRole = reaction.message.guild.roles.find(r => r.name === role.name);
 
 	if (!serverRole) return;
 
@@ -117,27 +140,34 @@ client.on("messageReactionRemove", async (reaction, user) => {
 	await user.send(`I removed you from the "${serverRole.name}" role.`);
 });
 // Roles reaction updates
-client.on("messageUpdate", async (_, message) => {
+const rolesReactions = async message => {
+	if (message.author.bot) return;
+
 	if (message.channel.name !== "roles") return;
 
-	const emojis = message.content
-		.split("\n")
-		.splice(2)
-		.map(text => text.split(" ")[0]);
+	const roles = roleExtractor(message);
 
-	emojis.forEach(emoji => {
-		if (message.reactions.find(e => e.emoji.name === emoji)) return;
+	roles.forEach(({ emoji }) => {
+		if (message.reactions.find(e => e.emoji.name === (emoji.name || emoji)))
+			return;
 
 		message.react(emoji);
 	});
 
-	message.reactions.forEach(async e => {
-		if (emojis.includes(e.emoji.name)) return;
+	message.reactions.forEach(async reaction => {
+		if (
+			roles.find(
+				role => reaction.emoji.name === (role.emoji.name || role.emoji)
+			)
+		)
+			return;
 
-		const users = await e.fetchUsers();
+		const users = await reaction.fetchUsers();
 
-		users.forEach(u => e.remove(u.id));
+		users.forEach(user => reaction.remove(user.id));
 	});
-});
+};
+client.on("message", rolesReactions);
+client.on("messageUpdate", (_, message) => rolesReactions(message));
 
 client.login(process.env.DISCORD_TOKEN);
