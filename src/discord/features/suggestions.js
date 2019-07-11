ac2a231e0aa661aa0484e3609b434d4e4c9e98b1
Alvaro Fresquet <alvarofresquet@gmail.com>
2019-07-11 03:07:00 -0300

    Suggestions fix

--- src/discord/features/suggestions.js
+++ src/discord/features/suggestions.js
@@ -1,6 +1,9 @@
 import isProduction from "../../helpers/isProduction";
 
 export default client => {
+	const yes = "✅";
+	const no = "❌";
+
 	client.on("message", async message => {
 		if (!isProduction) return;
 
@@ -8,7 +11,31 @@ export default client => {
 
 		if (message.channel.name !== "suggestions") return;
 
-		await message.react("✅");
-		await message.react("❌");
+		await message.react(yes);
+		await message.react(no);
+	});
+
+	client.on("messageReactionAdd", (reaction, user) => {
+		if (!isProduction) return;
+
+		if (user.bot) return;
+
+		if (reaction.message.channel.name !== "suggestions") return;
+
+		const emoji = reaction.emoji.name;
+
+		if (emoji !== yes && emoji !== no) return;
+
+		const oppositeEmoji = emoji === yes ? no : yes;
+
+		const oppositeReaction = reaction.message.reactions.get(oppositeEmoji);
+
+		const hasUserReactedToOpposite = oppositeReaction.users.find(
+			u => u.id === user.id
+		);
+
+		if (hasUserReactedToOpposite) {
+			oppositeReaction.remove(user.id);
+		}
 	});
 };
