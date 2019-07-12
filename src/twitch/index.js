ec688e8695149bcb1dbae337d7b49b71ecd28c18
Alvaro Fresquet <alvarofresquet@gmail.com>
2019-07-12 20:27:50 -0300

    Twitch Bot setup

---
+++ src/twitch/index.js
@@ -0,0 +1,42 @@
+import TMI from "tmi.js";
+
+export default (prod, logger, TWITCH_BOT_PASSWORD) => {
+	const client = new TMI.client({
+		options: { debug: false },
+		connection: {
+			secure: true,
+			reconnect: true
+		},
+		identity: {
+			username: "valaxor_bot",
+			password: `oauth:${TWITCH_BOT_PASSWORD}`
+		},
+		channels: ["valaxor_"]
+	});
+
+	client.prod = prod;
+
+	client.logToDiscord = (content, error) =>
+		logger(
+			{
+				author: {
+					name: "Twitch",
+					icon_url:
+						"https://seeklogo.com/images/T/twitch-tv-logo-51C922E0F0-seeklogo.com.png"
+				},
+				color: "#473978",
+				thumbnail:
+					"https://seeklogo.com/images/T/twitch-tv-logo-51C922E0F0-seeklogo.com.png",
+				...content
+			},
+			error
+		);
+
+	client.on("connected", () => {
+		client.action(client.opts.channels[0], "is online! valaxoSmile");
+
+		client.logToDiscord({ description: "I logged in to Twitch!" });
+	});
+
+	return client;
+};
