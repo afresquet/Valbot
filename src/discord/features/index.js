7845b5c63bfae7d333c4f40f1380a546448fe18d
afresquet <alvarofresquet@gmail.com>
2020-03-30 15:21:25 +0200

    wiped everything

--- src/discord/features/index.js
+++
@@ -1,2 +0,0 @@
-export { default as roles } from "./roles";
-export { default as suggestions } from "./suggestions";
--- src/twitch/helpers/index.js
+++
@@ -1,2 +0,0 @@
-export { default as onErrorHandler } from "./onErrorHandler";
-export { default as logToDiscord } from "./logToDiscord";
