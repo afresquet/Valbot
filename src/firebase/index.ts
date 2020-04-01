240f91dbaf7bf62b7d8c76d91f18494d4d6a3d7a
afresquet <alvarofresquet@gmail.com>
2020-04-02 01:09:37 +0200

    Build setup

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,15 +1,20 @@
 import admin from "firebase-admin";
-import path from "path";
 import { isProduction } from "../helpers/isProduction";
+
+const clientEmail = isProduction
+	? process.env.FIREBASE_CLIENT_EMAIL
+	: process.env.FIREBASE_DEV_CLIENT_EMAIL;
+
+const privateKey = isProduction
+	? process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
+	: process.env.FIREBASE_DEV_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
 const projectId = isProduction
 	? process.env.FIREBASE_PROJECT_ID
 	: process.env.FIREBASE_DEV_PROJECT_ID;
 
 admin.initializeApp({
-	credential: admin.credential.cert(
-		path.join(__dirname, `../../${projectId}-firebase-cert.json`)
-	),
+	credential: admin.credential.cert({ clientEmail, privateKey, projectId }),
 	databaseURL: `https://${projectId}.firebaseio.com/`,
 });
 
