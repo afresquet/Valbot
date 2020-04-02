ff9c5e03a7da8826793e647c133ce7708b5eedda
afresquet <alvarofresquet@gmail.com>
2020-04-02 05:57:55 +0200

    Discord live feature

--- src/discord/features/index.ts
+++ src/discord/features/index.ts
@@ -1,10 +1,11 @@
 import Discord from "discord.js";
 import tmi from "tmi.js";
 import { DiscordFeature } from "../../types/Feature";
+import { live } from "./live";
 import { reactions } from "./reactions";
 import { roles } from "./roles";
 
-export const features: DiscordFeature[] = [reactions, roles];
+export const features: DiscordFeature[] = [live, reactions, roles];
 
 export const applyDiscordFeatures = (
 	discord: Discord.Client,
