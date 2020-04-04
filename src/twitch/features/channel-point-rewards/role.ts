ff2707816223611ddfbc5972f3292cfadf77ae45
afresquet <alvarofresquet@gmail.com>
2020-04-05 01:49:39 +0200

    Twitch role reward feature

---
+++ src/twitch/features/channel-point-rewards/role.ts
@@ -0,0 +1,61 @@
+import { usernameRegex } from "../../../discord/helpers/regex";
+import { TwitchFeature } from "../../../types/Feature";
+import { PubSubListener } from "../../../types/PubSubListener";
+
+const rewardNameRegex = /^(.+\S)\s*role in my Discord server/;
+
+export const role: TwitchFeature = (twitch, discord) => {
+	const listener: PubSubListener = async (channel, userstate, redemption) => {
+		if (!rewardNameRegex.test(redemption.rewardName)) return;
+
+		if (!usernameRegex.test(redemption.message)) {
+			await twitch.say(
+				channel,
+				`@${userstate.name}, you didn't provide a valid Discord username in your message!`
+			);
+
+			return;
+		}
+
+		// TODO: make this scalable IF I'm ever opening the bot to many users.
+		const guild = discord.guilds.cache.first();
+
+		const discordUsername = usernameRegex.exec(redemption.message)![1];
+
+		const user = guild?.members.cache.find(
+			m => m.user.tag.toLowerCase() === discordUsername.toLowerCase()
+		);
+
+		if (!user) {
+			await twitch.say(
+				channel,
+				`@${userstate.name}, there is no member in the server with such username!`
+			);
+
+			return;
+		}
+
+		const roleName = rewardNameRegex.exec(redemption.rewardName)![1];
+
+		const role = guild?.roles.cache.find(
+			r => r.name === roleName.toLowerCase()
+		);
+
+		if (!role) {
+			await twitch.say(
+				channel,
+				`@${userstate.name}, there is no such role in the Discord, Val fucked up valKEKW`
+			);
+
+			return;
+		}
+
+		await user.roles.add(role);
+
+		await twitch.say(
+			channel,
+			`@${userstate.name}, the role was given successfully! Check Discord!`
+		);
+	};
+	twitch.on("pubsub" as any, listener);
+};
