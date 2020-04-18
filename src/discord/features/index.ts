74ceedf5fe09530827d779dca8f47b06e792e4ca
afresquet <alvarofresquet@gmail.com>
2020-04-18 16:33:19 +0200

    Audio files

--- src/discord/features/index.ts
+++ src/discord/features/index.ts
@@ -4,8 +4,9 @@ import { DiscordFeature } from "../../types/Feature";
 import { live } from "./live";
 import { reactions } from "./reactions";
 import { roles } from "./roles";
+import { werewolf } from "./werewolf";
 
-export const features: DiscordFeature[] = [live, reactions, roles];
+export const features: DiscordFeature[] = [live, reactions, roles, werewolf];
 
 export const applyDiscordFeatures = (
 	discord: Discord.Client,
