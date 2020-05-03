69fc30c25e58affaf3d52d4d48e392dceb131afb
afresquet <alvarofresquet@gmail.com>
2020-05-03 12:49:28 +0200

    Doppelganger handle night action

--- src/discord/features/werewolf/characters/Doppelganger.ts
+++ src/discord/features/werewolf/characters/Doppelganger.ts
@@ -1,9 +1,12 @@
 import Discord from "discord.js";
 import { characters } from ".";
+import { delay } from "../../../../helpers/delay";
 import { Character } from "../Character";
+import { centerEmojis, numberEmojis } from "../emojis";
 import { findPlayerById } from "../helpers/findPlayerById";
 import { listOfEveryone } from "../helpers/listOfEveryone";
 import { Player } from "../Player";
+import { Sound } from "../Sounds";
 import { CharacterModel } from "./CharacterModel";
 
 export class Doppelganger extends CharacterModel {
@@ -15,6 +18,109 @@ export class Doppelganger extends CharacterModel {
 
 	nightAction = true;
 	protected playerReactionsDM = true;
+
+	async handleNightAction(
+		players: Player[],
+		centerCards: Character[],
+		roleDelay: number,
+		playSound: (character: Character, sound: Sound) => Promise<void>,
+		createEmbed: (options: Discord.MessageEmbedOptions) => Discord.MessageEmbed
+	) {
+		if (this.amount <= 0) return;
+
+		const doppelganger = players.find(
+			player => player.role === this.name
+		) as Player<Character.DOPPELGANGER>;
+
+		await playSound(this.name, Sound.WAKE);
+
+		this.privateMessage = await doppelganger?.member.send(
+			createEmbed(this.nightActionDM(doppelganger, players))
+		);
+
+		if (this.playerReactionsDM) {
+			for (let i = 0; i < players.length; i++) {
+				if (doppelganger?.member.id === players[i].member.id) continue;
+
+				await this.privateMessage?.react(numberEmojis[i]);
+			}
+		}
+
+		await delay(roleDelay);
+
+		await this.privateMessage?.delete();
+
+		delete this.privateMessage;
+
+		if (doppelganger?.action) {
+			doppelganger.action.ready = true;
+		}
+
+		const copiedRole = characters.get(doppelganger?.action?.role?.character);
+
+		if (
+			[
+				Character.SEER,
+				Character.ROBBER,
+				Character.TROUBLEMAKER,
+				Character.DRUNK,
+			].includes(doppelganger?.action?.role?.character)
+		) {
+			this.privateMessage = await doppelganger?.member.send(
+				createEmbed(
+					copiedRole!.nightActionDM(doppelganger, players, centerCards)
+				)
+			);
+		}
+
+		if (
+			[Character.SEER, Character.ROBBER, Character.TROUBLEMAKER].includes(
+				doppelganger?.action?.role?.character
+			)
+		) {
+			for (let i = 0; i < players.length; i++) {
+				if (doppelganger?.member.id === players[i].member.id) continue;
+
+				await this.privateMessage?.react(numberEmojis[i]);
+			}
+		}
+
+		if (
+			[Character.SEER, Character.DRUNK].includes(
+				doppelganger?.action?.role?.character
+			)
+		) {
+			for (let i = 0; i < centerEmojis.length; i++) {
+				await this.privateMessage?.react(centerEmojis[i]);
+			}
+		}
+
+		await delay(roleDelay);
+
+		await this.privateMessage?.delete();
+
+		delete this.privateMessage;
+
+		const minion = characters.get(Character.MINION)!;
+
+		if (minion.amount > 0) {
+			await playSound(Character.MINION, Sound.DOPPELGANGER);
+
+			if (doppelganger?.action?.role?.character === Character.MINION) {
+				this.privateMessage = await doppelganger.member.send(
+					createEmbed(minion.nightActionDM(doppelganger, players))
+				);
+			}
+
+			await delay(roleDelay);
+
+			await this.privateMessage?.delete();
+
+			delete this.privateMessage;
+		}
+
+		await playSound(this.name, Sound.CLOSE);
+	}
 
 	nightActionDM(
 		player: Player,
