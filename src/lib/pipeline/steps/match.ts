b269f12eed987b6e25efef67f3ad00c4f032f292
Alvaro Fresquet <alvarofresquet@gmail.com>
2022-05-11 15:05:33 +0200

    pipeline lib match step

---
+++ src/lib/pipeline/steps/match.ts
@@ -0,0 +1,59 @@
+import { Pipeline } from "../pipeline";
+
+interface Match<T, R, C> {
+	run(value: T, context: C): R | PromiseLike<R>;
+
+	on(
+		matcher: (value: T, context: C) => boolean,
+		pipeline?: Pipeline.Step<T, R, C>
+	): Match<T, R, C>;
+
+	otherwise(pipeline: Pipeline.Step<T, R, C>): Match<T, R, C>;
+}
+
+class MatchBuilder<T, R, C> implements Match<T, R, C> {
+	private matchers: {
+		matcher: Pipeline.Step<T, boolean, C>;
+		pipeline: Pipeline.Step<T, R, C>;
+	}[] = [];
+
+	private otherwisePipeline?: Pipeline.Step<T, R, C>;
+
+	run(value: T, context: C): R | PromiseLike<R> {
+		for (const { matcher, pipeline } of this.matchers) {
+			if (matcher(value, context) && pipeline) {
+				return pipeline(value, context);
+			}
+		}
+
+		if (!this.otherwisePipeline) {
+			throw new Error("No 'otherwise' pipeline defined for unmatched value");
+		}
+
+		return this.otherwisePipeline(value, context);
+	}
+
+	on(matcher: Pipeline.Step<T, boolean, C>, pipeline?: Pipeline.Step<T, R, C>) {
+		if (pipeline) {
+			this.matchers.push({ matcher, pipeline });
+		}
+
+		return this;
+	}
+
+	otherwise(pipeline: Pipeline.Step<T, R, C>) {
+		this.otherwisePipeline = pipeline;
+
+		return this;
+	}
+}
+
+export function match<R, T, C>(
+	matchFn: (m: Match<T, R, C>) => void
+): Pipeline.Step<T, R, C> {
+	const builder = new MatchBuilder<T, R, C>();
+
+	matchFn(builder);
+
+	return builder.run.bind(builder);
+}
