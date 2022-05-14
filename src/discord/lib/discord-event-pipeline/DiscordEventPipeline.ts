0df87cc07e242c5d89bb3aa8a87e0acd40e87485
Alvaro Fresquet <alvarofresquet@gmail.com>
2022-05-14 03:14:55 +0200

    removed unnecessary generic from discord pipeline builder

--- src/discord/lib/discord-event-pipeline/DiscordEventPipeline.ts
+++ src/discord/lib/discord-event-pipeline/DiscordEventPipeline.ts
@@ -5,9 +5,8 @@ import { DiscordEventPipeline } from "./discord-event-pipeline";
 
 export default class DiscordEventPipelineBuilder<
 	Event extends keyof ClientEventsContext,
-	Initial = ClientEventsContext[Event],
 	Value = ClientEventsContext[Event]
-> extends PipelineBuilder<Initial, Value, ClientEventsContext[Event], Context> {
+> extends PipelineBuilder<Value, Value, ClientEventsContext[Event], Context> {
 	static CommandInteraction = class DiscordCommandInteractionPipelineBuilder<
 		Value = DiscordEventPipeline.CommandInteraction.Event
 	> extends PipelineBuilder<
