c753255c58dffa357a2d3234e57337d50b220fb6
Alvaro Fresquet <alvarofresquet@gmail.com>
2022-05-14 04:07:45 +0200

    pipeline async type checking 🎉🎉🎉

--- src/lib/pipeline/Pipeline.ts
+++ src/lib/pipeline/Pipeline.ts
@@ -1,31 +1,46 @@
 import { Query } from "mongoose";
 import { isPromise } from "util/types";
-import type { Pipeline } from "./pipeline";
+import type { IsAsync, IsPromise, Persist, Pipeline } from "./pipeline";
 
-export default class PipelineBuilder<Input, Current, Context, Global>
-	implements Pipeline.PipelineBuilder<Input, Current, Context, Global>
+export default class PipelineBuilder<
+	Input,
+	Current,
+	Context,
+	Global,
+	Async = false
+> implements Pipeline.PipelineBuilder<Input, Current, Context, Global, Async>
 {
 	fns: Pipeline.Pipeline<any, any, Context, Global>[] = [];
 
-	pipe<Next>(
+	pipe<Next, IsAsync = IsPromise<Next>>(
 		fn: Pipeline.Pipeline<Current, Next, Context, Global>
-	): PipelineBuilder<Input, Next, Context, Global> {
+	) {
 		this.fns.push(fn);
 
-		return this as unknown as PipelineBuilder<Input, Next, Context, Global>;
+		return this as unknown as PipelineBuilder<
+			Input,
+			Awaited<Next>,
+			Context,
+			Global,
+			Persist<Async, IsAsync>
+		>;
 	}
 
-	done(): Pipeline.Pipeline<Input, Current, Context, Global> {
-		const composition: Pipeline.Pipeline<Input, Current, Context, Global> =
-			this.fns.reduce((fn1, fn2) => (value, context, global) => {
-				const res = fn1(value, context, global);
+	done(): Pipeline.Pipeline<Input, IsAsync<Current, Async>, Context, Global> {
+		const composition: Pipeline.Pipeline<
+			Input,
+			IsAsync<Current, Async>,
+			Context,
+			Global
+		> = this.fns.reduce((fn1, fn2) => (value, context, global) => {
+			const res = fn1(value, context, global);
 
-				if (isPromise(res) || res instanceof Query) {
-					return res.then(r => fn2(r, context, global));
-				}
+			if (isPromise(res) || res instanceof Query) {
+				return res.then(r => fn2(r, context, global));
+			}
 
-				return fn2(res, context, global);
-			});
+			return fn2(res, context, global);
+		});
 
 		return (value, context, global) => composition(value, context, global);
 	}
--- src/lib/pipeline/pipeline.d.ts
+++ src/lib/pipeline/pipeline.d.ts
@@ -1,15 +1,21 @@
+export type IsPromise<T> = T extends PromiseLike<any> ? true : false;
+
+export type Persist<T, U> = T extends true ? T : U;
+
+export type IsAsync<T, B> = B extends true ? Promise<T> : T;
+
 export declare namespace Pipeline {
 	interface Pipeline<Value, Result, Context, Global> {
-		(value: Value, context: Context, global: Global): Result | Promise<Result>;
+		(value: Value, context: Context, global: Global): Result;
 	}
 
-	interface PipelineBuilder<Input, Current, Context, Global> {
+	interface PipelineBuilder<Input, Current, Context, Global, Async = false> {
 		fns: Pipeline<any, any, Context, Global>[];
 
-		pipe<Next>(
+		pipe<Next, IsAsync = IsPromise<Next>>(
 			pipeline: Pipeline<Current, Next, Context, Global>
-		): PipelineBuilder<Input, Next, Context, Global>;
+		): PipelineBuilder<Input, Next, Context, Global, Persist<Async, IsAsync>>;
 
-		done(): Pipeline<Input, Current, Context, Global>;
+		done(): Pipeline<Input, IsAsync<Current, Async>, Context, Global>;
 	}
 }
